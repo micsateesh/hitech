8fa3b0134969e606d65e03cfce3bc4589b123882
micsateesh <mic.sateesh@gmail.com>
2017-07-12 23:01:39 +0530

    View updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var sindex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    sindex++;
+    if (sindex > x.length) {sindex = 1}
+    x[sindex - 1].style.display = "block";
+    setTimeout(carousel, 1000); // Change image every 2 seconds
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
